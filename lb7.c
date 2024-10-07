33a99a327b70728300569592b3aa8c223cfb821a
Ugriumova <vikaezhevika1408@gmail.com>
2024-10-07 12:52:03 +0300

    Add files via upload

---
+++ lb7.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+char main()
+{
+	
+	setlocale(LC_ALL, "RUS");
+	//задание1
+	/*char c;
+	printf("Введите символ '1' или 'a': ");
+	scanf_s ("%c", c);
+	switch (c)
+	{
+	case '1':
+		printf("Введена цифра\n");
+		break;
+	case 'a':
+		printf("Введена буква\n");
+		break;
+	default:
+		printf("Неизвестный символ\n");
+	}*/
+
+	//задание 2(калькулятор)
+	
+	float x, y;
+	char c1;
+	printf("Введите число \n");
+	scanf_s("%f%c%f", &x,&c1 , &y);
+	
+	
+	switch (c1)
+	{
+	case '+':
+		printf(" = %f", (x + y));
+		break;
+	case '-':
+		printf(" = %f", (x - y));
+		break;
+	case '*':
+		printf(" = %f", (x * y));
+		break;
+	case '/':
+		printf(" = %f", (x / y));
+		break;
+	case '^':
+		printf("= %f", pow(x,y));
+		break;
+	default:
+		printf("Неизвестный символ\n");
+	}
+	return 0;
+
+}
